77a53cf037c53bb8f3e56f4247eefa35f1f95370
huwenchao <huwenchao@sensetime.com>
2021-07-27 22:56:50 +0800

    测试

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,20 @@
+/*
+ * @Date: 2021-07-27 19:59:04
+ * @LastEditors: pdshwc
+ * @LastEditTime: 2021-07-27 22:44:39
+ */
+module.exports = {
+  lang: "zh-CN",
+  title: "你好， VuePress ！",
+  description: "这是我的第一个 VuePress 站点",
+
+  themeConfig: {
+    logo: "https://vuejs.org/images/logo.png",
+    // 如果你按照 `organization/repository` 的格式设置它
+    // 我们会将它作为一个 GitHub 仓库
+    // 你也可以直接将它设置为一个 URL
+    repo: "pdshwc/vuepress-test",
+    docsDir: "docs",
+    base:'/vuepress/'
+  },
+};
